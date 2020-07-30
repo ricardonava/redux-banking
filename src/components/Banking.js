ad8b593c6fd985cbd003b59fda162d34aaeae05b
Ricardo <ricardo.a.nava@gmail.com>
2020-07-29 20:14:09 -0700

    Add working balance functionality

--- src/components/Banking.js
+++ src/components/Banking.js
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 export default function Banking() {
+  const [amount, setAmount] = useState('');
+  const dispatch = useDispatch();
+
+  const handleDeposit = () => {
+    dispatch({ type: 'DEPOSIT', amount: parseInt(amount, 10) });
+  };
+  const handleWithdraw = () => {
+    dispatch({ type: 'WITHDRAW', amount: parseInt(amount, 10) });
+  };
+
+  const handleCollectInterest = () => {
+    dispatch({ type: 'COLLECT_INTEREST' });
+  };
+
+  const handledDelete = () => {
+    dispatch({ type: 'DELETE_ACCOUNT' });
+  };
+
+  const handleChangeAccount = () => {};
+
   return (
     <div className="form-group">
-      <input type="text" className="form-control" />
-      <button type="button" className="btn btn-success">
+      <input
+        onChange={(e) => setAmount(e.target.value)}
+        type="text"
+        className="form-control"
+      />
+      <button onClick={handleDeposit} type="button" className="btn btn-success">
         Deposit
       </button>
-      <button type="button" className="btn btn-primary">
+      <button
+        onClick={handleWithdraw}
+        type="button"
+        className="btn btn-primary"
+      >
         Withdraw
       </button>
-      <button type="button" className="btn btn-warning">
+      <button
+        onClick={handleCollectInterest}
+        type="button"
+        className="btn btn-warning"
+      >
         Collect Interest
       </button>
-      <button type="button" className="btn btn-danger">
+      <button onClick={handledDelete} type="button" className="btn btn-danger">
         Delete Account
       </button>
-      <button type="button" className="btn btn-secondary">
+      <button
+        onClick={handleChangeAccount}
+        type="button"
+        className="btn btn-secondary"
+      >
         Change Account Type
       </button>
     </div>
