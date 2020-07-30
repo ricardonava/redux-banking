24f2783a9257a32e7d04549fcaa03c79b2bd3930
Ricardo <ricardo.a.nava@gmail.com>
2020-07-29 19:37:00 -0700

    Create bankingReducer for the main balance logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from 'redux';
 import App from './App';
+import { bankingReducer } from './reducers/bankingReducers';
+
+const store = createStore(bankingReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/reducers/bankingReducers.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/prefer-default-export */
+const initialState = 0;
+
+export const bankingReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'DEPOSIT':
+      return state + action.amount;
+    case 'WITHDRAW':
+      return state - action.amount;
+    case 'COLLECT_INTEREST':
+      return state * 1.03;
+    case 'DELETE_ACCOUNT':
+      return 0;
+
+    default:
+      return state;
+  }
+};
